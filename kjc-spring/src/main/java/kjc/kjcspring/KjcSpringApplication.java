9c488a45ade5cb722f0237afbc38c6b222308db7
JaeChang <wockd3325@gmail.com>
2021-03-07 00:33:55 +0900

    spring gradle 프로젝트 생성

---
+++ kjc-spring/src/main/java/kjc/kjcspring/KjcSpringApplication.java
@@ -0,0 +1,13 @@
+package kjc.kjcspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KjcSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KjcSpringApplication.class, args);
+	}
+
+}
---
+++ kjc-spring/src/test/java/kjc/kjcspring/KjcSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package kjc.kjcspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KjcSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
